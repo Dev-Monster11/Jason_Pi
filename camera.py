49387ca90d9af5c2f9bef3b9d2c5a43316e91250
Dev-Monster11 <softdrink1991@gmail.com>
2022-11-10 02:09:02 +0300

    Camera Test

--- camera.py
+++ camera.py
@@ -1,13 +1,25 @@
 import cv2
-import time
-cap = cv2.VideoCapture(0)
-
-while True:
-	ret, frame = cap.read()
-
-	if not ret:
-		
-		break
-	cv2.imshow("frame", frame)
-	time.sleep(0.04)
-cap.release()
+  
+  
+# define a video capture object
+vid = cv2.VideoCapture(0)
+  
+while(True):
+      
+    # Capture the video frame
+    # by frame
+    ret, frame = vid.read()
+  
+    # Display the resulting frame
+    cv2.imshow('frame', frame)
+      
+    # the 'q' button is set as the
+    # quitting button you may use any
+    # desired button of your choice
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+  
+# After the loop release the cap object
+vid.release()
+# Destroy all the windows
+cv2.destroyAllWindows()
