4285ffc0c729838faadcf37998f50db2011f01cd
Dev-Monster11 <softdrink1991@gmail.com>
2022-11-10 02:36:10 +0300

    Camera Test

--- camera.py
+++ camera.py
@@ -3,7 +3,8 @@ import cv2
   
 # define a video capture object
 vid = cv2.VideoCapture("udpsrc port=5000 ! gdpdepay ! rtph264depay ! avdec_h264 ! videoconvert ! appsink sync=false")
-  
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+out = cv2.VideoWriter('output.avi', fourcc, 20.0, (1280, 720))
 while(True):
       
     # Capture the video frame
@@ -11,7 +12,9 @@ while(True):
     ret, frame = vid.read()
   
     # Display the resulting frame
-    cv2.imshow('frame', frame)
+    if not ret:
+        break
+    out.write(frame)
       
     # the 'q' button is set as the
     # quitting button you may use any
@@ -21,5 +24,6 @@ while(True):
   
 # After the loop release the cap object
 vid.release()
+out.release()
 # Destroy all the windows
 cv2.destroyAllWindows()
