535fcdb4de1053bcd2288a77f24be17250c21071
Dev-Monster11 <softdrink1991@gmail.com>
2022-11-10 02:38:32 +0300

    Camera Test

--- camera.py
+++ camera.py
@@ -3,7 +3,7 @@ import cv2
   
 # define a video capture object
 vid = cv2.VideoCapture("udpsrc port=5000 ! gdpdepay ! rtph264depay ! avdec_h264 ! videoconvert ! appsink sync=false")
-fourcc = cv2.VideoWriter_fourcc(*'XVID')
+fourcc = cv2.VideoWriter_fourcc(*'H264')
 out = cv2.VideoWriter('output.avi', fourcc, 20.0, (1280, 720))
 while(True):
       
