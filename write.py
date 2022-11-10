ff589fdd0c322d14ecceeaf78f7000451dd842a5
Dev-Monster11 <softdrink1991@gmail.com>
2022-11-10 23:00:00 +0300

    skeleton

--- write.py
+++ write.py
@@ -73,42 +73,70 @@ def main():
     config = loadConfig()
 
     start = time.time()
-    camera = MyThread(0)
+    # camera = MyThread(0)
     camera.startThread()
     
-    seconds = [0]
-    temp = 0
-    for content in config['contents']:
-        temp = temp + content['AdDuration']
-        seconds.append(temp)
-    # print(seconds)
-    # exit(0)
+    # seconds = [0]
+    # temp = 0
+    # for content in config['contents']:
+    #     temp = temp + content['AdDuration']
+    #     seconds.append(temp)
+    # # print(seconds)
+    # # exit(0)
     start = time.time()
-    tempStart = start    
+    tempStart = start
+    cap = cv2.VideoCapture(0)
+    adSize = (0, 0)
+    if (config['layout'] == 'left_50' or config['layout'] == 'right_50'):
+        adSize = (int(width * 0.5), height)
+    else:
+        adSize = (width, int(height * 0.1))
+    contentShow = False
     while(True):
+
         now = time.time()
         
-        delta = (int(now - tempStart / 1000)) % seconds[len(seconds) - 1]
 
-        index = 0
-        contentFrame = np.zeros((width, height, 3), dtype = "uint8")
-        for x in seconds:
-            if (delta < x):
-                break
-            index = index + 1
+        if (now - start) % config['period'] == 0:
+            contentShow = True
+        # index = 0
+        # contentFrame = np.zeros((width, height, 3), dtype = "uint8")
+        # for x in seconds:
+        #     if (delta < x):
+        #         break
+        #     index = index + 1
 
-        data = config['contents'][index - 1]
-        if (data['AdType'] == 'VIDEO'):
-            content = camThread(data['AdPath'])
-            content.start()
-            contentFrame = content.frame
-        elif (data['AdType'] == 'IMAGE'):
-            url_response = urllib.request.urlopen(data['AdPath'])
-            contentFrame = cv2.imdecode(np.array(bytearray(url_response.read()), dtype=np.uint8), -1)
-        elif (data['AdType'] == 'SCROLL'):
-            cv2.putText(contentFrame, data['AdPath'], (width / 2, height / 2), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255, 255, 255))
+        # data = config['contents'][index - 1]
+        # if (data['AdType'] == 'VIDEO'):
+        #     content = camThread(data['AdPath'])
+        #     content.start()
+        #     contentFrame = content.frame
+        # elif (data['AdType'] == 'IMAGE'):
+        #     url_response = urllib.request.urlopen(data['AdPath'])
+        #     contentFrame = cv2.imdecode(np.array(bytearray(url_response.read()), dtype=np.uint8), -1)
+        # elif (data['AdType'] == 'SCROLL'):
+        #     cv2.putText(contentFrame, data['AdPath'], (width / 2, height / 2), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255, 255, 255))
 
-        buildFrame(config['layout'], camera.frame, contentFrame)
+        # buildFrame(config['layout'], camera.frame, contentFrame)
+
+        _, frame = cv2.read()
+        if contentShow:
+            index = random.randint(0, len(config['contents']) - 1)
+            data = config['contents'][index]
+
+            if (data['AdType'] == 'IMAGE'):
+                url_response = urllib.request.urlopen(data['AdPath'])
+                contentFrame = cv2.imdecode(np.array(bytearray(url_response.read()), dtype=np.uint8), -1)
+                contentFrame = cv2.resize(contentFrame, adSize)
+                if (data['layout'] == 'left_50'):
+                    frame[0:height, 0:int(width/2)] = contentFrame
+                elif (data['layout'] == 'right_50'):
+                    frame[0:height, int(width/2):width] = contentFrame
+                elif (data['layout'] == 'top_10'):
+                    frame[0:int(height/10), 0:width] = contentFrame
+                elif (data['layout'] == 'bottom_10'):
+                    frame[int(height/10):height, 0:width] = contentFrame
+            cv2.imshow('frame', frame)
         try:
             if cv2.waitKey(1) == ord('q'):
                 break
