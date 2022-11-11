c2fbbd68ad3c68402fa9a11223db39f4662cd92d
Dev-Monster11 <softdrink1991@gmail.com>
2022-11-11 03:56:04 +0300

    SaveAVI

---
+++ saveavi.py
@@ -0,0 +1,19 @@
+import cv2
+fourcc = cv2.VideoWriter_fourcc(*'XVID')
+out = cv2.VideoWriter('../output.avi', fourcc, 25.0, (1920, 1080))
+
+def main():
+	index = 0
+	while True:
+		img = cv2.imread("{0}.jpg".format(index))
+		out.write(img)
+		cv.waitKey(1)
+		if (index % 100 == 0):
+			print(index / 100)
+		if index == 15000:
+			break
+		index = index + 1
+	out.release()
+	exit(1)
+
+
